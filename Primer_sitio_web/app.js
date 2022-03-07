f1fbe30b79f1dd74e28c9ddb766e2130353f2b26
Edgar Martinez <88914174+ALPHHONSE2@users.noreply.github.com>
2022-03-06 23:53:46 -0600

    Add files via upload

---
+++ Primer_sitio_web/app.js
@@ -0,0 +1,14 @@
+nombre = "juan"
+
+document.write("<h1>este es mi script</h1>")
+console.log({ "nombre" : "edgar ", "edad": 22})
+console.log(nombre)
+
+function cambio(){
+
+    /*document.getElementById("subtitulo").style.display = "titulo";*/
+    /*document.getElementById("subtitulo").className = "titulo";*/
+    /*document.getElementById("subtitulo").innerHTML = "Hello";
+    el.className = "start";*/
+    document.getElementsByClassName("start").innerHTML = "HELLO";
+}
